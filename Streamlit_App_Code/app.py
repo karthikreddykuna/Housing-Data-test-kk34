c46031b1cdf6f0c603e912dbfed03104a4f9d04a
root <root@52fa01556d1d>
2022-02-02 02:47:23 +0000

    DXC_AI_STARTER STREAMLIT CHANGES

--- Streamlit_App_Code/app.py
+++ Streamlit_App_Code/app.py
@@ -23,7 +23,7 @@ exported_pipeline.fit(training_features, training_target)
 
 ######################
 # User defined values
-title = 'Housing-Data for Streamlit'
+title = 'Housing-Data Demo for Streamlit'
 encoder_location = 'https://github.com/karthikreddykuna/Housing-Data-test-kk34/blob/master/Streamlit_App_Code/encoder.pkl?raw=true'
 target_encoder_location = ''
 if len(encoder_location) > 5:
